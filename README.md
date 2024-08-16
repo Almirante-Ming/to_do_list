14248d1293f01bc429ef16db50361a183f05241e
v. guilherme <v.guilherme.barreto@gmail.com>
2024-08-16 10:45:51 -0400

    att no app.js

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 um to do list feito com javascript como metodo de avaliacao para a disciplina de web dinamico
+
+segue modelo
+
+![image](https://github.com/user-attachments/assets/383f16bc-6cd6-416d-bd71-0b11df032d4e)
--- app/app.js
+++ app/app.js
@@ -1,16 +1,24 @@
 // capturar elementos
+const container = document.getElementById('container');
 const form = document.querySelector('#form_storage');
 const input = document.querySelector('#txt_bar');
+const select = document.querySelector('#priority');
+const date_time = document.querySelector('#remain_time');
 
 
 // eventos
 form.addEventListener('submit', function(event){
     event.preventDefault();
-    const name = input.value;
-    localStorage.setItem("nome", JSON.stringify({nome: name,idade:20}))
-    const r = localStorage.getItem("nome");
+    const task = input.value;
+    const priority = select.value;
+    const rmt = date_time.value;
+    localStorage.setItem("task", JSON.stringify({task: task, priority, rmt}))
+    const r = localStorage.getItem("task");
     console.log(r)
 })
 
+// priority.addEventListener('change', function(event){
+//     console.log(priority.value)
+// })
 
 // funcoes
