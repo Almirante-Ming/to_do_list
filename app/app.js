a8f318a996e0f76f9b9740546f96de3a62069dbc
V. Guilherme <v.guilherme.barreto@gmail.com>
2024-08-20 10:39:59 -0400

    correcao da funcao map, verificacao de tasks como array, remocao das aspas no appenchild
    a funcao map que antes estava com erro por os dados de entrada nao ser do tipo string, mas sim um objeto, com ajuda de codigos de testes, foi possivel verificar que 'tasks' estava sendo inializado como um objeto, ao inves do array de objetos esperados pela constante data_pool, bem como a correcao do appenchild que estava com strings declaradas, nao criando as divs necessarias no html.

--- app/app.js
+++ app/app.js
@@ -1,51 +1,69 @@
-// capturar elementos
+// Capturar elementos
 const container = document.getElementById('container');
+const tsk_lst = document.getElementById('tsk_lst');
 const form = document.querySelector('#form_storage');
 const input = document.querySelector('#txt_bar');
 const select = document.querySelector('#priority');
 const date_time = document.querySelector('#remain_time');
 
 
-// eventos
-form.addEventListener('submit', function(event){
+// Eventos
+form.addEventListener('submit', function(event) {
     event.preventDefault();
-    const task = input.value;
+    
+    const task_name = input.value;
     const priority = select.value;
     const rmt = date_time.value;
-    localStorage.setItem("task", JSON.stringify({task: task, priority, rmt}))
-    const r = localStorage.getItem("task");
-    console.log(r)
-})
+
+    let task = [{ task_name: task_name, priority: priority, rmt: rmt }];
+    let tasks = JSON.parse(localStorage.getItem("task"));
+
+    if (!Array.isArray(tasks)) {
+        tasks = [];
+    }
+
+    tasks.push(task);
+    localStorage.setItem("task", JSON.stringify(tasks));
+    criar_lista();
+});
 
 // priority.addEventListener('change', function(event){
-    //     console.log(priority.value)
-// })
+//     console.log(priority.value);
+// });
 
-// funcoes
-const data_pool= localStorage.getItem("task");
 
-// async function getResponse(){
-
-//     data_pool.map((post)=> {
-//         const divCard= document.createElement('div');
-//         divCard.classList.add("card");
-//         const task_name=document.createElement('h1');
-//         task_name.innerText=post.title
-//         const remain_time=document.createElement('p');
-//         remain_time.innerText=post.body
-//         const concluido=document.createElement('a');
-//         concluido.innerText="ler mais..."
-//         link.setAttribute("href", `https://jsonplaceholder.typicode.com/posts/${post.id}/comments`)
-//         link.setAttribute("id", post.id )
+// Funções
+function criar_lista() {
+    const data_sto = localStorage.getItem("task");
+    let data_pool = JSON.parse(data_sto);
     
-//         divCard.appendChild(title);
-//         divCard.appendChild(body);
-//         divCard.appendChild(link);
-//         container.appendChild(divCard);
+    // if (!Array.isArray(data_pool)) {
+    //     console.error('data_pool não é um array:', data_pool);
+    //     return;
+    // }
+    
+    tsk_lst.innerHTML = '';
+
+    data_pool.map((post) => {
+        const divCard = document.createElement('div');
+        divCard.classList.add('task');
         
-//         console.log(post)
-// } )
-//  }
-
-
-//  getResponse();
+        const task_name = document.createElement('h2');
+        task_name.innerText = post.task_name;
+        
+        const level = document.createElement('p');
+        level.innerText = post.priority;
+        
+        const date_limit = document.createElement('input');
+        date_limit.classList.add('rmt');
+        date_limit.value = post.rmt;
+        
+        divCard.appendChild(task_name);
+        divCard.appendChild(level);
+        divCard.appendChild(date_limit);
+        tsk_lst.appendChild(divCard);
+        
+        // console.log(post);
+    });
+}
+criar_lista();
