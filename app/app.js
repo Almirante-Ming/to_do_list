2292bfb49038cd654e26e37b751fb604fbf83c64
v. guilherme <v.guilherme.barreto@gmail.com>
2024-08-28 00:05:47 -0400

    crud ok

--- app/app.js
+++ app/app.js
@@ -6,31 +6,24 @@ const input = document.querySelector('#txt_bar');
 const select = document.querySelector('#priority');
 const date_time = document.querySelector('#remain_time');
 
-
 // Eventos
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    
-    const task_name = input.value;
-    const priority = select.value;
-    const rmt = date_time.value;
-
-    let task = { task_name: task_name, priority: priority, rmt: rmt };
-    let tasks = JSON.parse(localStorage.getItem("task")) || [];
-
-    tasks.push(task);
-    localStorage.setItem("task", JSON.stringify(tasks));
-    active_task();
-});
-
 tsk_lst.addEventListener('click', function(event) {
     if (event.target && event.target.classList.contains('finish-btn')) {
         const taskIndex = event.target.dataset.index;
         finish_task(taskIndex);
+    } else if (event.target && event.target.classList.contains('edit-btn')) {
+        const taskIndex = event.target.dataset.index;
+        edit_task(taskIndex);
+    } else if (event.target && event.target.classList.contains('delete-btn')) {
+        const taskIndex = event.target.dataset.index;
+        delete_task(taskIndex);
     }
 });
 
-// Funcoes
+
+form.addEventListener('submit', listener);
+
+//funcoes
 function active_task() {
     const data_sto = localStorage.getItem("task");
     let data_pool = JSON.parse(data_sto) || [];
@@ -38,15 +31,13 @@ function active_task() {
     tsk_lst.innerHTML = '';
     
     data_pool.reverse().forEach((post, index) => {
+
         const divCard = document.createElement('div');
         divCard.classList.add('task');
-        
         const task_name = document.createElement('h2');
         task_name.innerText = post.task_name;
-        
         const level = document.createElement('p');
         level.innerText = post.priority;
-        
         const date_limit = document.createElement('input');
         date_limit.classList.add('rmt');
         date_limit.value = post.rmt;
@@ -56,24 +47,108 @@ function active_task() {
         finish.value = 'concluir';
         finish.classList.add('finish-btn');
         finish.setAttribute('data-index', index);
+        
+        const edit = document.createElement('input');
+        edit.setAttribute('type', 'button');
+        edit.value = 'editar';
+        edit.classList.add('edit-btn');
+        edit.setAttribute('data-index', index);
 
+        const del = document.createElement('input');
+        del.setAttribute('type', 'button');
+        del.value = 'excluir';
+        del.classList.add('delete-btn');
+        del.setAttribute('data-index', index);
+        
         divCard.appendChild(task_name);
         divCard.appendChild(level);
         divCard.appendChild(date_limit);
         divCard.appendChild(finish);
+        divCard.appendChild(edit);
+        divCard.appendChild(del);
         tsk_lst.appendChild(divCard);
     });
+}
+
+function listener(event) {
+    event.preventDefault();
+
+    const task_name = input.value;
+    const priority = select.value;
+    const rmt = date_time.value;
+
+    let task = {
+        task_name: task_name,
+        priority: priority,
+        rmt: rmt
+    };
+    let tasks = JSON.parse(localStorage.getItem("task")) || [];
+
+    tasks.push(task);
+    localStorage.setItem("task", JSON.stringify(tasks));
+    active_task();
+
+    input.value = '';
+    select.value = '';
+    date_time.value = '';
+}
+
+function edit_task(index) {
+    let tasks = JSON.parse(localStorage.getItem("task")) || [];
+    
+    if (index >= 0 && index < tasks.length) {
+        input.value = tasks[index].task_name;
+        select.value = tasks[index].priority;
+        date_time.value = tasks[index].rmt;
+        
+
+        form.removeEventListener('submit', listener);
+        
+        function saveEdit(event) {
+            event.preventDefault();
+
+            tasks[index].task_name = input.value;
+            tasks[index].priority = select.value;
+            tasks[index].rmt = date_time.value;
+
+            localStorage.setItem("task", JSON.stringify(tasks));
+
+            form.removeEventListener('submit', saveEdit);
+            form.addEventListener('submit', listener);
+
+            input.value = '';
+            select.value = '';
+            date_time.value = '';
+
+            active_task();
+        }
+
+        form.addEventListener('submit', saveEdit);
+    }
+    else {
+         alert('Tarefa não encontrada para edição.');
+    }
 }
 
 function finish_task(index) {
     let tasks = JSON.parse(localStorage.getItem("task")) || [];
     let finishedTasks = JSON.parse(localStorage.getItem("finish_tasks")) || [];
-
+    
     const [completedTask] = tasks.splice(index, 1);
     finishedTasks.push(completedTask);
-
+    
     localStorage.setItem("task", JSON.stringify(tasks));
     localStorage.setItem("finish_tasks", JSON.stringify(finishedTasks));
+    
+    active_task();
+}
+
+function delete_task(index) {
+    let tasks = JSON.parse(localStorage.getItem("task")) || [];
+
+    tasks.splice(index, 1);
+
+    localStorage.setItem("task", JSON.stringify(tasks));
 
     active_task();
 }
